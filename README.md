57b8481113c7f4e26de7aea8988abfe9aa6f2697
smbek <mbek.kg@gmail.com>
2024-04-18 23:55:27 +0000

    New files

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # terraform-aws-vpcbatch5
+
+module "vpc" {
+    source = "smbek/terraform-aws-vpcbatch5/aws"
+    region        = "us-east-2"
+    vpc_cidr      = "10.0.0.0/16"
+    sub1_cidr     = "10.0.1.0/24"
+    sub2_cidr     = "10.0.2.0/24"
+    sub3_cidr     = "10.0.3.0/24"
+    ip_on_launch   = true
+    instance_type = "t2.micro"
+}
---
+++ ec2.tf
@@ -0,0 +1,30 @@
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.main1.id
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  user_data              = file("apache.sh")
+
+}
+
+output "ec2" {
+  value = aws_instance.web.public_ip
+
+}
